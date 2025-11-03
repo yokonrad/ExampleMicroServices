9b8ebcc8578174a882985f0059dbad913ff1c2ef
kopuchlik <konrad.opuchlik@gomez.pl>
2025-11-03 23:27:07 +0100

    Added core domain test project and its implementation

---
+++ tests/Core/Core.Domain.Tests/Entities/EntityTest.cs
@@ -0,0 +1,50 @@
+﻿using AutoBogus;
+using Core.Domain.Entities;
+using FluentAssertions;
+using Moq;
+
+namespace Core.Domain.Tests.Entities;
+
+public class EntityTest
+{
+    [Test]
+    public void Should_Throw_ArgumentException_When_Guid_Any()
+    {
+        // Arrange
+        var fakerEntity = new AutoFaker<Entity>()
+            .RuleFor(x => x.Guid, _ => It.IsAny<Guid>());
+
+        // Act
+        var act = () => fakerEntity.Generate();
+
+        // Assert
+        act.Should().Throw<ArgumentException>();
+    }
+
+    [Test]
+    public void Should_Throw_ArgumentException_When_Guid_Empty()
+    {
+        // Arrange
+        var fakerEntity = new AutoFaker<Entity>()
+            .RuleFor(x => x.Guid, _ => Guid.Empty);
+
+        // Act
+        var act = () => fakerEntity.Generate();
+
+        // Assert
+        act.Should().Throw<ArgumentException>();
+    }
+
+    [Test]
+    public void Should_Be_Valid()
+    {
+        // Arrange
+        var fakerEntity = new AutoFaker<Entity>();
+
+        // Act
+        var act = fakerEntity.Generate();
+
+        // Assert
+        act.Should().BeOfType<Entity>().And.NotBeNull();
+    }
+}
