d0d167807c86732a69e786d72287f9a21bd37214
kopuchlik <konrad.opuchlik@gomez.pl>
2025-11-04 00:40:20 +0100

    Added core infrastructure project and its implementation

---
+++ src/Core/Core.Infrastructure/Extensions/ProgramExtension.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Core.Infrastructure.Extensions;
+
+public static class ProgramExtension
+{
+    public static IServiceCollection AddEntityFrameworkCoreSupport<T>(this IServiceCollection serviceCollection, Action<DbContextOptionsBuilder> actionDbContextOptionsBuilder) where T : DbContext
+    {
+        serviceCollection.AddDbContext<T>(actionDbContextOptionsBuilder);
+
+        return serviceCollection;
+    }
+
+    public static IServiceProvider AddEntityFrameworkCoreSupport<T>(this IServiceProvider serviceProvider) where T : DbContext
+    {
+        using var scope = serviceProvider.CreateScope();
+        using var dbContext = scope.ServiceProvider.GetRequiredService<T>();
+
+        dbContext.Database.EnsureCreated();
+
+        return serviceProvider;
+    }
+}
