577c2924f414b7fae5e0c21c268c397bb72b07e5
kopuchlik <konrad.opuchlik@gomez.pl>
2025-11-03 23:26:31 +0100

    Added core domain project and its implementation

---
+++ src/Core/Core.Domain/Entities/Entity.cs
@@ -0,0 +1,8 @@
+﻿using Ardalis.GuardClauses;
+
+namespace Core.Domain.Entities;
+
+public record Entity()
+{
+    public required Guid Guid { get; set => field = Guard.Against.NullOrEmpty(value); }
+}
