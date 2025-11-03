82605542b0864c15160561b3be0cc711a8c3619f
kopuchlik <konrad.opuchlik@gomez.pl>
2025-11-03 18:51:07 +0100

    Added core webapi project and its implementation

---
+++ src/Core/Core.WebAPI/Extensions/ProgramExtension.cs
@@ -0,0 +1,66 @@
+﻿using FastEndpoints;
+using FastEndpoints.Swagger;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.DependencyInjection;
+using System.Text.Json;
+using System.Text.Json.Serialization;
+
+namespace Core.WebAPI.Extensions;
+
+public static class ProgramExtension
+{
+    public static WebApplicationBuilder AddHealthChecksSupport(this WebApplicationBuilder webApplicationBuilder)
+    {
+        webApplicationBuilder.Services.AddEndpointsApiExplorer();
+        webApplicationBuilder.Services.AddHealthChecks();
+
+        return webApplicationBuilder;
+    }
+
+    public static WebApplication AddHealthChecksSupport(this WebApplication webApplication)
+    {
+        webApplication.MapHealthChecks("/health").RequireHost("localhost");
+        webApplication.UseRouting();
+        webApplication.UseEndpoints(_ => { });
+
+        return webApplication;
+    }
+
+    public static WebApplicationBuilder AddFastEndpointsSupport(this WebApplicationBuilder webApplicationBuilder, string title = "API", string version = "v1")
+    {
+        webApplicationBuilder.Services.AddFastEndpoints();
+        webApplicationBuilder.Services.SwaggerDocument(o =>
+        {
+            o.DocumentSettings = s =>
+            {
+                s.Title = title;
+                s.Version = version;
+                s.MarkNonNullablePropsAsRequired();
+            };
+
+            o.SerializerSettings = s =>
+            {
+                s.Converters.Add(new JsonStringEnumConverter());
+                s.PropertyNamingPolicy = JsonNamingPolicy.CamelCase;
+            };
+
+            o.EnableJWTBearerAuth = false;
+            o.RemoveEmptyRequestSchema = true;
+            o.ShortSchemaNames = true;
+        });
+
+        return webApplicationBuilder;
+    }
+
+    public static WebApplication AddFastEndpointsSupport(this WebApplication webApplication, string version = "v1")
+    {
+        webApplication.UseFastEndpoints(c =>
+        {
+            c.Endpoints.RoutePrefix = $"api/{version}";
+            c.Serializer.Options.PropertyNamingPolicy = JsonNamingPolicy.CamelCase;
+        });
+        webApplication.UseSwaggerGen();
+
+        return webApplication;
+    }
+}
