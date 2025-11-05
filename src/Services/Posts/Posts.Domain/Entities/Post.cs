6ccc531438c90cd74ec5de41c2ff6b1b1d3c4407
kopuchlik <konrad.opuchlik@gomez.pl>
2025-11-06 00:22:08 +0100

    Added posts domain project and its implementation

---
+++ src/Services/Posts/Posts.Domain/Entities/Post.cs
@@ -0,0 +1,11 @@
+﻿using Ardalis.GuardClauses;
+using Core.Domain.Entities;
+
+namespace Posts.Domain.Entities;
+
+public record Post() : Entity
+{
+    public required string Title { get; set => field = Guard.Against.NullOrWhiteSpace(value); }
+    public required string Text { get; set => field = Guard.Against.NullOrWhiteSpace(value); }
+    public required bool Visible { get; set; }
+}
---
+++ src/Services/Posts/Posts.Domain/Interfaces/IPostRepository.cs
@@ -0,0 +1,16 @@
+﻿using Posts.Domain.Entities;
+
+namespace Posts.Domain.Interfaces;
+
+public interface IPostRepository
+{
+    Task<Post[]> GetAsync(CancellationToken cancellationToken = default);
+
+    Task<Post?> GetByGuidAsync(Guid guid, CancellationToken cancellationToken = default);
+
+    Task<bool> CreateAsync(Post post, CancellationToken cancellationToken = default);
+
+    Task<bool> UpdateAsync(Post post, CancellationToken cancellationToken = default);
+
+    Task<bool> DeleteAsync(Post post, CancellationToken cancellationToken = default);
+}
