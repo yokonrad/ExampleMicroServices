ae1f3c31e2c162eee3935a8fa1a232ee597bdf83
kopuchlik <konrad.opuchlik@gomez.pl>
2025-11-06 00:22:21 +0100

    Added posts domain test project and its implementation

---
+++ tests/Services/Posts.Domain.Tests/Entities/PostTest.cs
@@ -0,0 +1,106 @@
+﻿using AutoBogus;
+using FluentAssertions;
+using Moq;
+using Posts.Domain.Entities;
+
+namespace Posts.Domain.Tests.Entities;
+
+public class PostTest
+{
+    [Test]
+    public void Should_Throw_ArgumentNullException_When_Title_Any()
+    {
+        // Arrange
+        var fakerPost = new AutoFaker<Post>()
+            .RuleFor(x => x.Title, _ => It.IsAny<string>());
+
+        // Act
+        var act = () => fakerPost.Generate();
+
+        // Assert
+        act.Should().Throw<ArgumentNullException>();
+    }
+
+    [Test]
+    public void Should_Throw_ArgumentException_When_Title_Empty()
+    {
+        // Arrange
+        var fakerPost = new AutoFaker<Post>()
+            .RuleFor(x => x.Title, _ => string.Empty);
+
+        // Act
+        var act = () => fakerPost.Generate();
+
+        // Assert
+        act.Should().Throw<ArgumentException>();
+    }
+
+    [Test]
+    public void Should_Throw_ArgumentException_When_Title_Whitespace()
+    {
+        // Arrange
+        var fakerPost = new AutoFaker<Post>()
+            .RuleFor(x => x.Title, _ => "");
+
+        // Act
+        var act = () => fakerPost.Generate();
+
+        // Assert
+        act.Should().Throw<ArgumentException>();
+    }
+
+    [Test]
+    public void Should_Throw_ArgumentNullException_When_Text_Any()
+    {
+        // Arrange
+        var fakerPost = new AutoFaker<Post>()
+            .RuleFor(x => x.Text, _ => It.IsAny<string>());
+
+        // Act
+        var act = () => fakerPost.Generate();
+
+        // Assert
+        act.Should().Throw<ArgumentNullException>();
+    }
+
+    [Test]
+    public void Should_Throw_ArgumentException_When_Text_Empty()
+    {
+        // Arrange
+        var fakerPost = new AutoFaker<Post>()
+            .RuleFor(x => x.Text, _ => string.Empty);
+
+        // Act
+        var act = () => fakerPost.Generate();
+
+        // Assert
+        act.Should().Throw<ArgumentException>();
+    }
+
+    [Test]
+    public void Should_Throw_ArgumentException_When_Text_Whitespace()
+    {
+        // Arrange
+        var fakerPost = new AutoFaker<Post>()
+            .RuleFor(x => x.Text, _ => "");
+
+        // Act
+        var act = () => fakerPost.Generate();
+
+        // Assert
+        act.Should().Throw<ArgumentException>();
+    }
+
+    [Test]
+    public void Should_Be_Valid()
+    {
+        // Arrange
+        var fakerPost = new AutoFaker<Post>();
+
+        // Act
+        var act = fakerPost.Generate();
+
+        // Assert
+        act.Should().BeOfType<Post>().And.NotBeNull();
+    }
+}
