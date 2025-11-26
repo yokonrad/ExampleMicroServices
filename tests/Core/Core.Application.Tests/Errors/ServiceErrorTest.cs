064f94c61f1ff6b7fe2f1c039f9be84915418ef8
kopuchlik <konrad.opuchlik@gomez.pl>
2025-11-26 18:13:20 +0100

    Changed implementation of core application test project

---
+++ tests/Core/Core.Application.Tests/Errors/ServiceErrorTest.cs
@@ -0,0 +1,21 @@
+﻿using AutoBogus;
+using Core.Application.Errors;
+using FluentAssertions;
+
+namespace Core.Application.Tests.Errors;
+
+public class ServiceErrorTest
+{
+    [Test]
+    public void Should_Be_Valid()
+    {
+        // Arrange
+        var fakerServiceError = new AutoFaker<ServiceError>();
+
+        // Act
+        var act = fakerServiceError.Generate();
+
+        // Assert
+        act.Should().BeOfType<ServiceError>().And.NotBeNull();
+    }
+}
